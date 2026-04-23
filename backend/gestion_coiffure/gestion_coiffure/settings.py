2a40812176086ac4eff37afa899828a0d7779018
Adama Programmeur <informaticiendoucoure@gmail.com>
2026-04-23 20:54:34 +0000

    ajouter url de l'app au projet

--- backend/gestion_coiffure/gestion_coiffure/settings.py
+++ backend/gestion_coiffure/gestion_coiffure/settings.py
@@ -29,16 +29,10 @@ DEBUG = str(config("DEBUG", default="False")).lower() in ("1", "true", "yes", "o
 # ALLOWED_HOSTS (piloté par .env)
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default="127.0.0.1,localhost,barber-pro-upue.onrender.com",
+    default="barberpro-scwn.onrender.com",
     cast=Csv(),
 )
 
-# CSRF_TRUSTED_ORIGINS (piloté par .env)
-CSRF_TRUSTED_ORIGINS = config(
-    "CSRF_TRUSTED_ORIGINS",
-    default="https://barber-pro-upue.onrender.com",
-    cast=Csv(),
-)
 
 # Si tu veux tester avec n'importe quel domaine temporairement :
 # ALLOWED_HOSTS = ["*"]  # seulement pour tests, pas sûr pour prod
