ed8fb4a653af94500626a77cbc15d696494b9bc7
Adama Programmeur <informaticiendoucoure@gmail.com>
2026-05-05 10:03:04 +0000

    update

--- gestion_coiffure/settings.py
+++ gestion_coiffure/settings.py
@@ -60,6 +60,10 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+# Autoriser l'URL Render pour les vérifications CSRF (Indispensable pour l'admin)
+if RENDER_EXTERNAL_HOSTNAME:
+    CSRF_TRUSTED_ORIGINS = [f"https://{RENDER_EXTERNAL_HOSTNAME}"]
+
 
 # =========================
 # Installed apps
