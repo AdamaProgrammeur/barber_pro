bcbe13399ee3ee21b9edf6a07a5f4702055a5a97
Adama Programmeur <informaticiendoucoure@gmail.com>
2026-05-08 20:59:21 +0000

    update

--- gestion_coiffure/settings.py
+++ gestion_coiffure/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # =========================
 # Paths
@@ -94,12 +95,23 @@ WSGI_APPLICATION = 'gestion_coiffure.wsgi.application'
 # =========================
 # Database
 # =========================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=not DEBUG
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # =========================
 # Django REST Framework
